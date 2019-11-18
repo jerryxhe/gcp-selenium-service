df723895bb1270d6cb42eb525fc3c5beb90d8edb
Jerry He <rebound1618@gmail.com>
2019-11-17 21:31:31 -0500

    bug fixes

--- selenium_worker.py
+++ selenium_worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from selenium import webdriver as wd
 from pyvirtualdisplay import Display
-import sys
+import sys,os
 os.environ['PATH'] += ':'+os.path.dirname(os.path.realpath(__file__))
 
 js = {
@@ -13,7 +13,7 @@ js = {
   }
 }
 
-if __name__="__main__":
+if __name__=="__main__":
   ev_url = sys.argv[1] if len(sys.argv)>=2 else "https://www.evernote.com/shard/s637/sh/d2d5c174-4f11-4ec7-9e39-626371f0471d/d3da04f4dfb15d8c755922f0c16c23f0"
   from time import time
   display = Display(visible=0, size=(1024, 768))
@@ -29,9 +29,8 @@ if __name__="__main__":
     except Exception as e:
       print(traceback.format_exc())
       print(str(e))
-   d.close()
-   display.stop()
-   end_time = time()
-   _ans['seconds_elapsed']= int((end_time-start_time)*1000)
-   import json
-   print(json.dumps(_ans))
+  d.close()
+  display.stop()
+  end_time = time()
+  _ans['seconds_elapsed']= int((end_time-start_time)*1000)
+  print(_ans)
